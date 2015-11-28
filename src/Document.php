bc5c42b50e61e9bd758ba6af486e8a8aed439ab4
Ben Scholzen <mail@dasprids.de>
2015-11-28 16:52:07 +0100

    Change copyright notice

--- src/Document.php
+++ src/Document.php
@@ -3,7 +3,7 @@
  * BaconPdf
  *
  * @link      http://github.com/Bacon/BaconPdf For the canonical source repository
- * @copyright 2015 Ben 'DASPRiD' Scholzen
+ * @copyright 2015 Ben Scholzen (DASPRiD)
  * @license   http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
 
