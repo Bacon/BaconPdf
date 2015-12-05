b7aafab766d7089fa2d5f00e1cf1e1a392f17c15
Ben Scholzen <mail@dasprids.de>
2015-12-05 15:01:48 +0100

    Add tests for NullEncryption

---
+++ test/Encryption/NullEncryptionTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * BaconPdf
+ *
+ * @link      http://github.com/Bacon/BaconPdf For the canonical source repository
+ * @copyright 2015 Ben Scholzen (DASPRiD)
+ * @license   http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ */
+
+namespace Bacon\PdfTest\Encryption;
+
+use Bacon\Pdf\Encryption\NullEncryption;
+use Bacon\PdfTest\TestHelper\MemoryObjectWriter;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @covers \Bacon\Pdf\Encryption\NullEncryption
+ */
+class NullEncryptionTest extends TestCase
+{
+    public function testEncryptReturnsPlaintext()
+    {
+        $encryption = new NullEncryption();
+        $this->assertSame('foo', $encryption->encrypt('foo', 1, 1));
+    }
+
+    public function writeEncryptDictionaryWritesNothing()
+    {
+        $encryption = new NullEncryption();
+        $objectWriter = new MemoryObjectWriter();
+        $encryption->writeEncryptDictionary($objectWriter);
+        $this->assertSame('', $writer->getData());
+    }
+}
